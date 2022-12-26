88149cae30e3d7c46f7b6b108206e362d0c1846a
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-12-26 07:39:55 +0100

    Collections object created

---
+++ collections-list/dist/scripts.js
@@ -0,0 +1,12 @@
+
+const imageCollections = [
+    {
+        "people":["https://cdn.shopify.com/s/files/1/1717/4865/files/tatiana-dietman_-aficionada_-fiest-bracelets_2048x2048.jpg?v=1626298269", "https://lebonas.com/wp-content/uploads/2019/05/GASHA.jpg", "https://www.tiktok.com/discover/rinyu-controller-officiel?lang=cs-CZ"]
+    },
+    {
+        "nature":["https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg","https://wallpaperaccess.com/full/2514219.jpg", "https://wallpapercave.com/wp/wp2829200.jpg", "https://wallpapercave.com/wp/wp2827632.jpg"]
+    },
+    {
+        "history":["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4agaN9f5NW1nVQgafGUC8oPieU4h8_vc4Zw&usqp=CAU", "https://th-thumbnailer.cdn-si-edu.com/uY9ax8lvcrsJ8grJgpHxKRza93c=/1400x1050/filters:focal(700x527:701x528)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/d0/fe/d0fe7c71-eb2c-4812-9109-d402f5987dcd/fascinating-finds.jpg", "https://thumbs.dreamstime.com/b/history-etched-old-paper-scroll-feather-quill-compass-30890377.jpg"]
+    }
+]
